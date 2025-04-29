7a81b6c75e3e1c23760a57aafacce9c63c1fc7c8
ebrahim <ebrahimasil@gmail.com>
2025-04-29 08:01:26 +0430

    Initial commit: set up project structure and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
+import Appointments from "./pages/Appointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div className="mx-3 md:mx-6 w-full xl:mx-auto xl:bg-blue-400 xl:max-w-screen-xl ">
+      <Router>
+        {/* navbar */}
+        <Navbar />
+
+        <Routes>
+          {/* public routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<Doctors />} />
+          <Route path="/doctors/:speciality" element={<Doctors />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* user routes */}
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/appointment/:docId" element={<Appointment />} />
+        </Routes>
+
+        {/* footer */}
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
